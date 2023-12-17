70da93444220be646d91dad74a2aaa1b70646d75
Avinish T Rao <32325947+avinishrao@users.noreply.github.com>
2023-12-17 16:53:56 +0100

    Initial Commit

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <div>.
+        <div><Navbar/> from the Home screen</div>
+        <div>This is the div of the Home page</div>
+    </div>
+    
+  )
+}
